6cd8cc2ba711fe59a72cc2473617a622e436bd44
Mario Aguiar <mario.aguiar@xwp.co>
2024-07-13 14:56:14 -0600

    Setup default variables and fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+/**
+ * Next dependencies
+ */
+import {
+    Merriweather,
+    Noto_Sans
+} from "next/font/google";
+
+export const merriweather = Merriweather({ subsets: ["latin"], weight: [ '400', '700' ] });
+export const notoSans = Noto_Sans({ subsets: ["latin"], weight: [ '400', '600' ] });
