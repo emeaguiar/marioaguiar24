c352d80e4dfd9d12c194da6f51bd3c960e4149d6
Mario Aguiar <mario.aguiar@xwp.co>
2024-07-15 14:49:14 -0600

    Allow reading data from existing blog posts

--- components/blog/cards.tsx
+++ components/blog/cards.tsx
@@ -5,20 +5,23 @@ import Image from "next/image";
 
 /**
  * Internal dependencies
- */
+*/
 import { merriweather } from "@/components/fonts";
+import type { PostItem } from "@/types/post";
 
-export default function Cards() {
+export default function Cards( { posts }: { [ key: string ]: any } ) {
     return (
         <div className="grid gap-8 items-center w-full lg:grid-cols-3 lg:max-w-screen-xl">
-            <Card />
-            <Card />
-            <Card />
+            {
+                posts && posts.map( ( post: PostItem ) => (
+                    <Card post={ post } key={ post.slug } />
+                ) )
+            }
         </div>
     );
 }
 
-function Card() {
+function Card( { post }: { post: any } ) {
     return (
         <div className="flex flex-col gap-4">
             <Image
@@ -30,11 +33,11 @@ function Card() {
 
             <div className="flex flex-col gap-4">
                 <h3 className={ `${ merriweather.className } font-bold text-xl` }>
-                    Phasellus sed metus blandit, suscipit metus eu, rhoncus.
+                    { post.title }
                 </h3>
 
                 <p>
-                    Pellentesque eu elit a dolor eleifend molestie. Integer eget turpis laoreet, convallis.
+                    { post.description }
                 </p>
             </div>
         </div>
