acaff57f29b04cfb6554bcc6a10e9d0eea520324
Mario Aguiar <mario.aguiar@xwp.co>
2024-07-18 07:58:40 -0600

    Add dark mode to alerts

--- components/alerts/index.tsx
+++ components/alerts/index.tsx
@@ -16,11 +16,11 @@ export default function Alert( { children, type }: {
             className={ clsx(
                 'p-4 my-4 border-l-4 rounded-lg lg:-mx-8',
                 {
-                    'border-blue-400 bg-blue-50': type === 'note',
-                    'border-green-400 bg-green-50': type === 'tip',
-                    'border-yellow-400 bg-yellow-50': type === 'important',
-                    'border-red-400 bg-red-50': type === 'warning',
-                    'border-purple-400 bg-purple-50': type === 'caution',
+                    'border-blue-400 bg-blue-50 text-blue-800 dark:border-blue-700 dark:bg-blue-950 dark:text-blue-100': type === 'note',
+                    'border-green-400 bg-green-50 text-green-800 dark:border-green-700 dark:bg-green-950 dark:text-green-100': type === 'tip',
+                    'border-yellow-400 bg-yellow-50 text-yellow-800 dark:border-yellow-700 dark:bg-yellow-950 dark:text-yellow-100': type === 'important',
+                    'border-red-400 bg-red-50 text-red-800 dark:border-red-400 dark:bg-red-950 dark:text-red-100': type === 'warning',
+                    'border-purple-400 bg-purple-50 text-purple-800 dark:border-purple-400 dark:bg-purple-950 dark:text-purple-100': type === 'caution',
                 }
             ) }
         >
