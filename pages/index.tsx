6c4f9cd6cac92e06fb56ce5fcc9eeff51fac4cfb
Mario Aguiar <mario.aguiar@xwp.co>
2024-07-19 15:41:10 -0600

    Update colors in cta button

--- pages/index.tsx
+++ pages/index.tsx
@@ -119,10 +119,10 @@ export default function Home( {
 
           <Link href="/?modal=1" className={
             clsx(
-              "flex items-center border border-red-700 bg-background font-bold px-4 py-2 rounded-lg text-white uppercase transition-colors",
+              "flex items-center border border-red-700 bg-background font-bold px-4 py-2 rounded-lg text-primary uppercase transition-colors",
               "hover:bg-red-50",
               "disabled:cursor-not-allowed disabled:bg-red-50 disabled:text-gray-500 disabled:border-gray-900",
-              "dark:border-slate-900 dark:hover:bg-slate-900"
+              "dark:border-slate-900 dark:hover:bg-slate-900 dark:text-white"
           )
           }>
             { t( 'common:contactCta' ) }
