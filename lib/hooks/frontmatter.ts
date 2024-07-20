28887bb1d4bbeb82f0e32288c85ada6fbddfcede
Mario Aguiar <mario.aguiar@xwp.co>
2024-07-20 12:39:02 -0600

    Add update frontmatter functions

---
+++ lib/hooks/frontmatter.ts
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { promises } from 'fs';
+import matter from 'gray-matter';
+
+const updateFrontmatter = async () => {
+  const [, , ...mdFilePaths] = process.argv;
+
+  mdFilePaths.forEach(async (path) => {
+    const file = matter.read(path);
+    const { data: currentFrontmatter } = file;
+
+    if (currentFrontmatter.published === true) {
+      const updatedFrontmatter = {
+        ...currentFrontmatter,
+        updatedOn: new Date().toISOString(),
+      };
+
+      file.data = updatedFrontmatter;
+
+      const updatedFileContent = matter.stringify(file);
+      promises.writeFile(path, updatedFileContent);
+    }
+  });
+};
+
+updateFrontmatter();
