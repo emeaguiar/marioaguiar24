8af56152a36038a9eb110d96805de2120006b666
Mario Aguiar <mario.aguiar@xwp.co>
2024-07-21 14:24:11 -0600

    Add first version of language switcher

--- components/blog/language-switcher.tsx
+++ components/blog/language-switcher.tsx
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import clsx from 'clsx';
 import useTranslation from 'next-translate/useTranslation';
 
 /**
@@ -18,7 +19,16 @@ export default function LanguageSwitcher({
   const { t } = useTranslation('blog');
 
   return (
-    <Link href={pathname} locale={locale === 'es' ? 'en' : 'es'}>
+    <Link
+      href={pathname}
+      locale={locale === 'es' ? 'en' : 'es'}
+      className={clsx(
+        'flex items-start rounded-lg border border-red-700 bg-background px-4 py-2 font-bold uppercase text-primary transition-colors',
+        'hover:bg-red-50',
+        'disabled:cursor-not-allowed disabled:border-gray-900 disabled:bg-red-50 disabled:text-gray-500',
+        'dark:border-slate-900 dark:text-white dark:hover:bg-slate-900'
+      )}
+    >
       {t('switchLanguage')}
     </Link>
   );
