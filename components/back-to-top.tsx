1e28075a4a749d44841d8a0cf93927c8f0a0605f
Mario Aguiar <mario.aguiar@xwp.co>
2024-07-22 16:14:25 -0600

    Style back to top and display only when needed

--- components/back-to-top.tsx
+++ components/back-to-top.tsx
@@ -1,12 +1,48 @@
+/**
+ * External dependencies
+ */
+import { useEffect, useState } from 'react';
+import { ArrowUpIcon } from '@heroicons/react/24/solid';
+import clsx from 'clsx';
+import useTranslation from 'next-translate/useTranslation';
+
 export default function BackToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+  const { t } = useTranslation('common');
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
   return (
     <button
+      aria-label={t('backToTop')}
       onClick={() => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
       }}
-      className='fixed bottom-0 right-0 z-50 m-4 rounded-lg bg-slate-200 p-4 text-blue-900 shadow-md'
+      className={clsx(
+        'fixed bottom-0 right-0 z-50 m-4 rounded-full bg-primary p-2 text-white shadow-lg ring-1 ring-secondary',
+        'transform-gpu transition-transform hover:scale-105',
+        'dark:bg-slate-700 dark:ring-slate-900',
+        {
+          'translate-y-16 opacity-0': !isVisible,
+          'translate-y-0 opacity-100': isVisible,
+        }
+      )}
     >
-      Back to top
+      <ArrowUpIcon className='h-8 w-8' />
     </button>
   );
 }
