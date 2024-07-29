0c17779c792364c001a00146e10c1f5a1232295d
Mario Aguiar <mario.aguiar@xwp.co>
2024-07-28 18:49:46 -0600

    Add type to variable

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -7,7 +7,7 @@ export default function SiteMap() {
   // This function will generate the sitemap.xml file
 }
 
-export async function getServerSideProps({ res }) {
+export async function getServerSideProps({ res }: { res: any }) {
   res.setHeader('Content-Type', 'text/xml');
 
   const sitemap = generateSiteMap();
