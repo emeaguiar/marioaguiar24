b28449dec2e00fdd50f0189a24d27f092e045f4e
Mario Aguiar <mario.aguiar@xwp.co>
2024-07-28 18:56:40 -0600

    Add xml tag attribute

--- lib/posts.ts
+++ lib/posts.ts
@@ -96,7 +96,7 @@ export function generateSiteMap() {
   const esPosts = getPosts('es');
 
   const siteMap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
       ${enPosts
         .map((post, index) => {
           return `
