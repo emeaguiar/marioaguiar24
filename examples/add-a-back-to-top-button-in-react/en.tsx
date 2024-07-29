d1b7c3f9d76cda35b12bb35f14dd023f0edf37d6
Mario Aguiar <mario.aguiar@xwp.co>
2024-07-28 21:13:40 -0600

    Add back to top post

---
+++ examples/add-a-back-to-top-button-in-react/en.tsx
@@ -0,0 +1,16 @@
+import { ArrowUpIcon } from '@heroicons/react/24/solid';
+
+export function SampleButton() {
+  return (
+    <button
+      onClick={() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }}
+      className='m-auto block self-center rounded-full bg-primary p-2 text-white shadow-lg ring-1 ring-secondary dark:bg-slate-700 dark:ring-slate-900'
+    >
+      <ArrowUpIcon className='h-6 w-6' />
+
+      <span className='sr-only'>Back to Top</span>
+    </button>
+  );
+}
