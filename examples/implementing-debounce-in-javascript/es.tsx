838a6a9918d6fa0de1fdccb2c35066f988349df0
Mario Aguiar <me@marioaguiar.net>
2024-10-17 10:38:38 -0600

    Add spanish post

---
+++ examples/implementing-debounce-in-javascript/es.tsx
@@ -0,0 +1,61 @@
+export function ExampleOne() {
+  return (
+    <form className='bg-slate-200 p-8' onSubmit={(e) => e.preventDefault()}>
+      <label
+        htmlFor='search1'
+        className='block text-sm font-medium text-gray-700'
+      >
+        Ingresa una palabra clave
+      </label>
+      <input
+        type='text'
+        id='search1'
+        name='search1'
+        className='mt-1 block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+        onChange={(e) => {
+          console.log(e.target.value);
+        }}
+      />
+    </form>
+  );
+}
+
+export function ExampleTwo() {
+  const debounce = (callback, delay) => {
+    // Guardamos referencia al timer.
+    let timer;
+
+    return (...args) => {
+      // Limpiamos el timer si existe.
+      clearTimeout(timer);
+
+      // Creamos un nuevo timer.
+      timer = setTimeout(() => {
+        // Ejecutamos la función con los argumentos.
+        callback(...args);
+      }, delay);
+    };
+  };
+
+  const debounced = debounce((e) => {
+    console.log(e.target.value);
+  }, 500);
+
+  return (
+    <form className='bg-slate-200 p-8' onSubmit={(e) => e.preventDefault()}>
+      <label
+        htmlFor='search1'
+        className='block text-sm font-medium text-gray-700'
+      >
+        Ingresa una palabra clave
+      </label>
+      <input
+        type='text'
+        id='search1'
+        name='search1'
+        className='mt-1 block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+        onChange={debounced}
+      />
+    </form>
+  );
+}
