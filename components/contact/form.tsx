cc2c01cb528fd61d6babefb1d630c2296d5343d2
Mario Aguiar <me@marioaguiar.net>
2024-11-02 13:18:28 -0600

    Simplify the use of fields in contact form

--- components/contact/form.tsx
+++ components/contact/form.tsx
@@ -11,6 +11,13 @@ import useTranslation from 'next-translate/useTranslation';
  */
 import Alert from '@/components/alerts';
 import { Spinner } from '@/components/icons';
+import { toNamespacedPath } from 'path';
+
+const FIELDS = [
+  { label: 'name', type: 'text' },
+  { label: 'email', type: 'email' },
+  { label: 'message', type: 'textarea' },
+];
 
 export default function ContactForm() {
   const [isLoading, setIsLoading] = useState(false);
@@ -64,52 +71,9 @@ export default function ContactForm() {
       className='mt-8 flex w-full flex-col items-start tracking-wide'
       onSubmit={handleOnSubmit}
     >
-      <label htmlFor='name' className='mb-2 text-xl font-black uppercase lg:text-3xl'>
-        {t('name')}:
-      </label>
-      <input
-        type='text'
-        id='name'
-        name='name'
-        required
-        className={clsx(
-          'mb-6 w-full rounded border border-gray-300 p-2 text-foreground',
-          'invalid:border-pink-500 invalid:text-pink-600',
-          'focus:invalid:border-pink-500 focus:invalid:ring-pink-500',
-          'dark:border-slate-900 dark:bg-white dark:text-black'
-        )}
-      />
-
-      <label htmlFor='email' className='mb-2 text-xl font-black uppercase lg:text-3xl'>
-        {t('email')}:
-      </label>
-      <input
-        type='email'
-        id='email'
-        name='email'
-        required
-        className={clsx(
-          'mb-6 w-full rounded border border-gray-300 p-2 text-foreground',
-          'invalid:border-pink-500 invalid:text-pink-600',
-          'focus:invalid:border-pink-500 focus:invalid:ring-pink-500',
-          'dark:border-slate-900 dark:bg-white dark:text-black'
-        )}
-      />
-
-      <label htmlFor='message' className='mb-2 text-xl font-black uppercase lg:text-3xl'>
-        {t('message')}:
-      </label>
-      <textarea
-        id='message'
-        name='message'
-        required
-        className={clsx(
-          'mb-6 min-h-44 w-full rounded border border-gray-300 p-2 text-foreground',
-          'invalid:border-pink-500 invalid:text-pink-600',
-          'focus:invalid:border-pink-500 focus:invalid:ring-pink-500',
-          'dark:border-slate-900 dark:bg-white dark:text-black'
-        )}
-      />
+      {FIELDS.map(({ label, type }) => (
+        <Input key={label} label={label} type={type} />
+      ))}
 
       {successMessage && (
         <div className='flex-column flex w-full justify-center'>
@@ -139,3 +103,35 @@ export default function ContactForm() {
     </form>
   );
 }
+
+const Input = ({label, type}: {
+  label: string;
+  type: string;
+}): React.ReactNode => {
+  const { t } = useTranslation('common');
+  const InputType = type === 'textarea' ? 'textarea' : 'input';
+
+  return (
+    <>
+      <label htmlFor='name' className='mb-2 text-xl font-black uppercase lg:text-3xl'>
+        {t(label)}:
+      </label>
+
+      <InputType
+        type={type}
+        id={label}
+        name={label}
+        required
+        className={clsx(
+          'mb-6 w-full rounded border border-gray-300 p-2 text-foreground',
+          'invalid:border-pink-500 invalid:text-pink-600',
+          'focus:invalid:border-pink-500 focus:invalid:ring-pink-500',
+          'dark:border-slate-900 dark:bg-white dark:text-black',
+          {
+            'min-h-44': type === 'textarea',
+          }
+        )}
+      />
+    </>
+  );
+};
