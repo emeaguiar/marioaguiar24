93f68d8f47f2efc5eb9b9209b2ed145f09a3914d
Mario Aguiar <me@marioaguiar.net>
2024-11-04 15:22:25 -0600

    Update 404 page and remove extra fonts

--- components/fonts.ts
+++ components/fonts.ts
@@ -1,36 +1,14 @@
 /**
  * Next dependencies
  */
-import {
-  Merriweather,
-  Long_Cang,
-  Luckiest_Guy,
-  Noto_Sans,
-  Noto_Sans_Mono,
-  Raleway,
-} from 'next/font/google';
+import { Long_Cang, Noto_Sans_Mono, Raleway } from 'next/font/google';
 
-export const merriweather = Merriweather({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-});
-export const notoSans = Noto_Sans({
-  subsets: ['latin'],
-  weight: ['400', '600'],
-  style: ['normal', 'italic'],
-});
 export const notoSansMono = Noto_Sans_Mono({
   subsets: ['latin'],
   weight: ['400'],
   style: ['normal'],
 });
 export const longCang = Long_Cang({
-  subsets: ['latin'],
-  weight: ['400'],
-  style: ['normal'],
-});
-export const luckiestGuySans = Luckiest_Guy({
   subsets: ['latin'],
   weight: ['400'],
   style: ['normal'],
--- pages/404.tsx
+++ pages/404.tsx
@@ -7,7 +7,6 @@ import { NextSeo } from 'next-seo';
 /**
  * Internal dependencies
  */
-import { luckiestGuySans } from '@/components/fonts';
 
 export default function Custom404() {
   const { t } = useTranslation('common');
@@ -17,15 +16,11 @@ export default function Custom404() {
       <NextSeo title={t('notFound')} description={t('pageNotFound')} noindex />
 
       <div className='flex flex-col items-center justify-center gap-40'>
-        <h1
-          className={`${luckiestGuySans.className} bg-gradient-to-t from-indigo-500 via-violet-500 to-purple-500 bg-clip-text p-4 text-7xl uppercase text-transparent [text-shadow:_1px_1px_10px_rgb(0_0_0_/_20%)] lg:text-9xl`}
-        >
+        <h1 className='p-4 text-7xl font-black uppercase md:text-8xl'>
           &lt;404 /&gt;
         </h1>
 
-        <h2
-          className={`${luckiestGuySans.className} bg-gradient-to-t from-indigo-500 via-violet-500 to-purple-500 bg-clip-text p-4 text-7xl uppercase text-transparent [text-shadow:_1px_1px_10px_rgb(0_0_0_/_20%)] lg:text-9xl`}
-        >
+        <h2 className='p-4 text-7xl font-black uppercase md:text-7xl'>
           {t('tooBad')}
         </h2>
         <p className='text-lg'>{t('pageNotFound')}</p>
