80d2b8ea1327cf1d11ab595429c916743150142d
Mario Aguiar <mario.aguiar@xwp.co>
2025-09-26 16:34:37 -0600

    Refactor ListItem component to improve layout and hover effects; add text truncation for better readability of post titles.

--- components/blog/list.tsx
+++ components/blog/list.tsx
@@ -34,7 +34,7 @@ export function ListItem({ post, locale }: { post: PostItem; locale: string }) {
   const { t } = useTranslation('blog');
 
   return (
-    <div className='group relative flex flex-row gap-4 border-b border-gray-200 py-2 last:border-b-0 overflow-hidden'>
+    <div className='group relative flex flex-row gap-4 overflow-hidden border-b border-gray-200 py-2 last:border-b-0'>
       <div className='flex flex-col gap-4'>
         <FormatDateShort
           dateString={post.publishedOn}
@@ -44,14 +44,14 @@ export function ListItem({ post, locale }: { post: PostItem; locale: string }) {
       </div>
       <h3
         className={clsx(
-          'transition-colors',
+          'truncate text-ellipsis transition-colors',
           'group-hover:text-zinc-900/60',
           'dark:hover:text-slate-300'
         )}
       >
         {post.title}
       </h3>
-      <div className='ml-auto mr-0 flex flex-row items-center gap-2 translate-x-4 group-hover:translate-x-0 transition-transform'>
+      <div className='ml-auto mr-0 flex translate-x-4 flex-row items-center gap-2 text-nowrap transition-transform group-hover:translate-x-0'>
         <p className='text-sm text-gray-500'>
           {t('blog:readingTime', { minutes: post.readingTime / 60 })}
         </p>
